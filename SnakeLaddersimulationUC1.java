4e81694becc4addc7305aa4664dc664cc444afdb
Deepanjan1498 <deepanjansharma43@gmail.com>
2020-09-26 18:17:11 +0530

    [Deepanjan]add . uc2 is solved

--- SnakeLaddersimulationUC1.java
+++
@@ -1,8 +0,0 @@
-public class SnakeLadderSimulationUC1
-{
-    public static void main(String args[])
-    {
-    int Player_Position=0;
-    System.out.println("The Start Position of Player is :"+Player_Position);
-	}
-}
---
+++ SnakeLaddersimulationUC2.java
@@ -0,0 +1,11 @@
+public class SnakeLadderSimulationUC2
+{
+   public static void main(String args[])
+   {
+    int Player_Position=0;
+    int Dice_Roll=(int)(Math.random())*6+1;
+    System.out.println("The value of Dice Roll is :"+Dice_Roll;
+    }
+}
+
+
