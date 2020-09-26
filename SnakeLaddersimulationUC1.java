74b986510df48f4fdc29ef2ec98d77e8230e84d5
Deepanjan1498 <deepanjansharma43@gmail.com>
2020-09-26 19:28:29 +0530

    [Deepanjan] add . Uc3 is solved

--- SnakeLaddersimulationUC1.java
+++ SnakeLaddersimulationUC1.java
@@ -1,11 +1,23 @@
 public class SnakeLadderSimulationUC1
 {
-   public static void main(String args[])
-   {
-    int Player_Position=0;
-    int Dice_Roll=(int)(Math.random())*6+1;
-    System.out.println("The value of Dice Roll is :"+Dice_Roll;
-    }
+   //Constants
+  static final int NoPlay=0;
+  static final int Ladder=1;
+  static final int Snake=2;
+  public static void main(String args[])
+  {
+   int position=0;
+   int diceroll=(int)(Math.random()*10)%6+1;
+   int option =(int)(Math.random()*10)%3;
+   if(option==NoPlay)
+       position=position+0;
+   else if(option==Ladder)
+       position=position+diceroll;
+   else
+       position=position-diceroll;
+System.out.println("The Player Position is "+position+" for the Diceroll of "+diceroll);
+   }
 }
+
 
 
