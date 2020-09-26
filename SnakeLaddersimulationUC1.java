f56040c5e1fd071b8c68f593cb2a1b979594c578
Deepanjan1498 <deepanjansharma43@gmail.com>
2020-09-26 18:02:58 +0530

    [Deepanjan] add .uc1 is solved.

---
+++ SnakeLaddersimulationUC1.java
@@ -0,0 +1,8 @@
+public class SnakeLadderSimulationUC1
+{
+    public static void main(String args[])
+    {
+    int Player_Position=0;
+    System.out.println("The Start Position of Player is :"+Player_Position);
+	}
+}
