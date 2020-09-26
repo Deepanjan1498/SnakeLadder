641b463175577b5396b315a2b137d7d8f4d7616a
Deepanjan1498 <deepanjansharma43@gmail.com>
2020-09-26 20:06:42 +0530

    [Deepanjan] add . uc7 is solved.

--- SnakeLaddersimulationUC1.java
+++ SnakeLaddersimulationUC1.java
@@ -5,22 +5,21 @@ public class SnakeLadderSimulationUC1
   static final int Ladder=1;
   static final int Snake=2;
   static final int Max=100;
-  public static void main(String args[])
-  {
-   int position=0;
-   int DiceCount=0;
-   while(position!=Max)
-   {
-      int diceroll=(int)(Math.random()*10)%6+1;
-      int option =(int)(Math.random()*10)%3;
-      DiceCount++;
-      if(option==NoPlay)
-        position=position+0;
-      else if(option==Ladder)
+  public static int pos(int position)
+{
+   int p=0; 
+   int diceroll=(int)(Math.random()*10)%6+1;
+   int option =(int)(Math.random()*10)%3;
+   if(option==NoPlay)
+     position=position+0;
+   else if(option==Ladder)
         {
           position=position+diceroll;
           if(position>Max)
-          position=position-diceroll;
+             position=position-diceroll;
+          else
+           p=0;
+           return pos(position);
          }
        else
         { 
@@ -28,13 +27,23 @@ public class SnakeLadderSimulationUC1
           if(position<0)
           position=0;
          }
-     System.out.println("Position after "+DiceCount+" is "+position);
-     }
-   System.out.println("The dice rolled for "+DiceCount+" times");
-   }
+       return position;
 }
 
-
-
-
-
+  public static void main(String args[])
+  {
+    int P1Position=0;
+    int P2Position=0;
+    int P1Initial,P2Initial,p1=0,p2=0;
+    System.out.println("Player 1 starts the game");
+    while(P1Position!=Max && P2Position!=Max)
+      {
+       P1Position=pos(P1Position);
+       P2Position=pos(P2Position);
+       }
+     if(P1Position==Max)
+        System.out.println("Player 1 wins the game");
+     if(P2Position==Max)
+        System.out.println("Player 2 wins the game");
+   }
+}
