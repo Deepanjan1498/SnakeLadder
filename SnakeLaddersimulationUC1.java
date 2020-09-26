f6cbe655785cbf1faea4fb85a882395deb038941
Deepanjan1498 <deepanjansharma43@gmail.com>
2020-09-26 19:37:55 +0530

    [Deepanjan] add . uc4 is solved.

--- SnakeLaddersimulationUC1.java
+++ SnakeLaddersimulationUC1.java
@@ -4,20 +4,29 @@ public class SnakeLadderSimulationUC1
   static final int NoPlay=0;
   static final int Ladder=1;
   static final int Snake=2;
+  static final int Max=100;
   public static void main(String args[])
   {
    int position=0;
-   int diceroll=(int)(Math.random()*10)%6+1;
-   int option =(int)(Math.random()*10)%3;
-   if(option==NoPlay)
-       position=position+0;
-   else if(option==Ladder)
-       position=position+diceroll;
-   else
-       position=position-diceroll;
-System.out.println("The Player Position is "+position+" for the Diceroll of "+diceroll);
+   while(position!=Max)
+   {
+      int diceroll=(int)(Math.random()*10)%6+1;
+      int option =(int)(Math.random()*10)%3;
+      if(option==NoPlay)
+        position=position+0;
+      else if(option==Ladder)
+        position=position+diceroll;
+      else
+        { 
+          position=position-diceroll;
+          if(position<0)
+          position=0;
+         }
+    }
+     System.out.println("The Player is playing the game");
    }
 }
+
 
 
 
